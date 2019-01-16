60be7c62b85b9048c44ce7cf9d2cad4f79a2a585
Alf Fenton <Fentonalf@gmail.com>
2019-01-16 17:15:35 -0500

    included searchable robot list and colored backgrounds

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './card';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import  { robots } from './robots';
